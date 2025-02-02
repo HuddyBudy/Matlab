3bd0fc93eae88813c78bb8483df58eb497a45bf6
HuddyBudy <151589670+HuddyBudy@users.noreply.github.com>
2025-02-02 01:04:51 -0800

    Update RadarSignalProcessingPersonalProject.m

--- RadarSignalProcessingPersonalProject.m
+++ RadarSignalProcessingPersonalProject.m
@@ -1,58 +1,81 @@
-%% Radar Signal Processing Personal project
-Print(' Please know what you are doing, I put unrealistic numbers and my computer froze')
+% Underwater Sonar Signal Processing
 
-% Radar Parmeters
-c = 3e8;           % Speed of light (m/s)
-fc = input('Enter your Carrier frequency in Hz ');           % Carrier frequency (Hz)
-PRF = input('Enter your Pulse repetition frequency in Hz '); % Pulse repetition frequency (Hz)
-Tp = input('Enter your Pulse width in seconds ');            % Pulse width (s)
-B = input('Enter your Bandwidth in Hz ');          % Bandwidth (Hz)
-fs = 2 * B;        % Sampling frequency (Hz)
+% Input Sonar parameters
+fc = input(' Enter the carrier frequency in Hz ( Like 10000 ): ');
+PRF = input('Enter the pulse repetition frequency in Hz ( Like 100 ): '); 
+PluseWidth = input('Enter the pulse width in seconds ( Like 0.01 s) : '); 
+Bandwith = input(' Enter the bandwidth in Hz ( Like 1000 ): '); 
+Range = input(' Enter the target range in meters ( Like 500 ): '); 
+velocity = input(' Enter the target velocity in meters per second ( like 10) : '); 
+c = 1500; % Speed of sound in water (m/s)
 
-t = 0:1/fs:Tp-1/fs; % Time vector for one pulse
-chirp_signal = exp(1j * pi * (B/Tp) * t.^2); % LFM chirp
+% Regular Sonar parameters
 
-
-R = input('Enter your target range in meters: ');          % Target range (m)
-v = input('Enter your target velocity in meters per second: ');            % Target velocity (m/s)
-tau = 2 * R / c;   % Time delay (s)
-fd = 2 * v * fc / c; % Doppler frequency (Hz)
-
-
-t_received = 0:1/fs:(Tp + tau)-1/fs; % Time vector for received signal This part gave me trouble
-received_signal = exp(1j * pi * (B/Tp) * (t_received - tau).^2) .* exp(1j * 2 * pi * fd * t_received);
-received_signal = awgn(received_signal, input('Enter your noise in decibels ')); % Add noise (SNR = 20 dB)
+% fc = 10000;
+% PRF = 100; 
+% PluseWidth = 0.01
+% Bandwith = 1000
+% Range = 500;
+% velocity = 10;
 
 
+% Generate transmitted chirp signal
+SamplingFreq = 2 * Bandwith; % Sampling frequency
+time = 0:1/SamplingFreq:PluseWidth-1/SamplingFreq; % Time vector for one pulse
+chirp_signal = exp(1j * pi * (Bandwith/PluseWidth) * time.^2); % LFM chirp
 
+% Simulate target reflection
+Tau = 2 * Range / c; % Time delay
+DopplerFreq = 2 * velocity * fc / c; % Doppler frequency
+TReceived = 0:1/SamplingFreq:(PluseWidth + Tau)-1/SamplingFreq; % Time vector for received signal
+RSignal = exp(1j * pi * (Bandwith/PluseWidth) * (TReceived - Tau).^2) .* exp(1j * 2 * pi * DopplerFreq * TReceived);
+
+% Add noise and attenuation to simulate underwater conditions
+RSignal = awgn(RSignal, 20); % Add noise (SNR = 20 dB)
+attenuation = 0.9; % Simulate signal attenuation in water
+RSignal = attenuation * RSignal;
+
+% Matched filtering
 matched_filter = conj(fliplr(chirp_signal)); % Matched filter
-output_signal = conv(received_signal, matched_filter, 'same'); % Convolution
+output_signal = conv(RSignal, matched_filter, 'same'); % Convolution
 
+% Range detection
+[~, peak_index] = max(abs(output_signal));
+range_estimate = (peak_index / SamplingFreq) * c / 2; % Estimated range
 
+% Doppler processing
 N = length(output_signal); % Number of samples
-f = (-fs/2:fs/N:fs/2-fs/N); % Frequency vector
+f = (-SamplingFreq/2:SamplingFreq/N:SamplingFreq/2-SamplingFreq/N); % Frequency vector
 doppler_spectrum = fftshift(fft(output_signal)); % FFT
 [~, doppler_index] = max(abs(doppler_spectrum));
 doppler_frequency = f(doppler_index); % Doppler frequency
 velocity_estimate = doppler_frequency * c / (2 * fc); % Estimated velocity
 
+% Display results
+disp(['Estimated range: ', num2str(range_estimate), ' meters']);
+disp(['Estimated velocity: ', num2str(velocity_estimate), ' m/s']);
 
-% Ploting
-N = length(output_signal); % Number of samples in outtput signal
-f = (-fs/2:fs/N:fs/2-fs/N); % Frequency vector
-doppler_spectrum = fftshift(fft(output_signal)); % FFT
-[~, doppler_index] = max(abs(doppler_spectrum));
-doppler_frequency = f(doppler_index); % Doppler frequency
-velocity_estimate = doppler_frequency * c / (2 * fc); % Estimated velocity
+% Visualization
+figure;
+subplot(2,1,1);
+plot(time, real(chirp_signal))
+title('Transmitted Chirp Signal');
+xlabel('Time (s)');
+ylabel('Amplitude');
 
-% Matched Filter Output plot
+subplot(2,1,2);
+plot(TReceived, real(RSignal))
+title('Received Signal (with noise and attenuation)');
+xlabel('Time (s)');
+ylabel('Amplitude');
+
 figure;
 plot(abs(output_signal))
-title('Matched Filter Output')
+title('Matched Filter Output');
 xlabel('Samples');
 ylabel('Amplitude');
 
-% figure;
+figure;
 plot(f, abs(doppler_spectrum))
 title('Doppler Spectrum');
 xlabel('Frequency (Hz)');
